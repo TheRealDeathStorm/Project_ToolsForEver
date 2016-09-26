6ef3e8122e571bd3efe11e428ea9fb597b7b4a4f
niels.van.enckevort <nielsvanenckevort@hotmail.com>
2016-09-26 12:29:30 +0200

    Index.php
    index startup

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Niels
+ * Date: 6-9-2016
+ * Time: 16:04
+ * het index.php voor ToolsForEver project:
+ */
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+
+    </body>
+</html>
