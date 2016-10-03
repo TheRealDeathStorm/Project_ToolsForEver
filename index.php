34fe14cdea594693cf1cfc11a98ae6e72bfcd76d
Mido63 <mikedorsman@hotmail.com>
2016-10-03 11:52:45 +0200

    Login Form
    formulier voor de login + css

--- index.php
+++ index.php
@@ -9,9 +9,32 @@
 ?>
 <html>
     <head>
-
+        <link rel="stylesheet" href="css/styles.css" type="text/css">
     </head>
+    <header>
+        <title>ToolsForEver</title>
+    </header>
     <body>
+    <form action="action_page.php">
+        <div class="imgcontainer">
+            <img src="" alt="Avatar" class="avatar">
+        </div>
 
+        <div class="container">
+            <label><b>Username</b></label>
+            <input type="text" placeholder="Enter Username" name="uname" required>
+
+            <label><b>Password</b></label>
+            <input type="password" placeholder="Enter Password" name="psw" required>
+
+            <button type="submit">Login</button>
+            <input type="checkbox" checked="checked"> Remember me
+        </div>
+
+        <div class="container" style="background-color:#f1f1f1">
+            <button type="button" class="cancelbtn">Cancel</button>
+            <span class="psw">Forgot <a href="#">password?</a></span>
+        </div>
+    </form>
     </body>
 </html>
