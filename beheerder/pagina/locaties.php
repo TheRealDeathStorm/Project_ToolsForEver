b3bba9618f069d9d6d745f5d690bb95870ffb906
Mido63 <mikedorsman@hotmail.com>
2016-10-13 16:06:22 +0200

    locaties
    locaties toegevoegd

--- beheerder/pagina/locaties.php
+++ beheerder/pagina/locaties.php
@@ -21,7 +21,40 @@ include "../DBconnect.php";
     ?>
 
     <div class="main-content">
+        <div class="locaties">
+            <form method="post">
+                <button type="submit" name="locaties">Locaties</button><br>
+                <?php
+                if(isset($_POST['locaties'])) {
+                    // Define the columns title and name in this array map.
+                    $columns = array(
+                        'Filiaal ID' => 'Filiaal_id',
+                        'Filiaal naam' => 'Filiaal_naam'
+                    );
+                    // Run the query
+                    $result = mysqli_query($db, "SELECT Filiaal_id, Filiaal_naam FROM filialen");
 
+                    // Output table header
+                    echo "<table border=\"1px solid black\" width=\"80%\"><tr>";
+                    foreach ($columns as $name => $col_name) {
+                        echo "<th>$name</th>";
+                    }
+                    echo "</tr>";
+
+                    // Output rows
+                    while ($row = mysqli_fetch_array($result)) {
+                        echo "<tr>";
+                        foreach ($columns as $name => $col_name) {
+                            echo "<td style=\"text-align:center;\">" . $row[$col_name] . "</td>";
+                        }
+                        echo "</tr>";
+                    }
+                    // Close table
+                    echo "</table>";
+                }
+                ?>
+            </form>
+        </div>
     </div>
 
     <div class="clearfix">
